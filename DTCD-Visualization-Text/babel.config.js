faa84dc9826bea168d550a7ffaed2a4c1b61e4cc
sbelikov98 <sbelikov@isgneuro.com>
2021-12-20 09:25:23 +0300

    Tests init

--- DTCD-Visualization-Text/babel.config.js
+++ DTCD-Visualization-Text/babel.config.js
@@ -4,8 +4,4 @@ module.exports = {
       '@babel/preset-env', { targets: { node: 'current' } },
     ],
   ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-private-methods',
-  ]
 };
